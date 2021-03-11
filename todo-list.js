6f50c1d43dc6548e2274d1f835339ea93e799082
sophiehu36 <sophiehu36@163.com>
2021-03-11 21:06:36 +0800

    Update todo-list.js
    增加功能：点击add todo新增todo task，点击垃圾桶图标删除todo，用localStorage保存todo列表。

--- todo-list.js
+++ todo-list.js
@@ -1,16 +1,109 @@
+//获取当前时间
 const currentTime = function() {
-    let d = new Date()
-    let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    let weekdayArray = ['Sunday', 'Monday', 'Tuesday', ' Wednesday', 'Thursday', 'Friday', 'Saturday']
-    let month = monthArray[d.getMonth()]
-    let date = d.getDate()
-    let weekday = weekdayArray[d.getDay()]
-    let timeString = `${weekday}, ${month} ${date}`
+    const d = new Date()
+    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const weekdayArray = ['Sunday', 'Monday', 'Tuesday', ' Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const month = monthArray[d.getMonth()]
+    const date = d.getDate()
+    const weekday = weekdayArray[d.getDay()]
+    const timeString = `${weekday}, ${month} ${date}`
     return timeString
 }
+//显示当前时间
 const showCurrentTime = function() {
-    let timeDiv = e('#current-time')
-    let time = currentTime()
+    const timeDiv = e('#current-time')
+    const time = currentTime()
     timeDiv.innerHTML = time
 }
-showCurrentTime()
+
+//把输入的todo保存起来
+//初始化todoList为一个空数组
+let todoList = []
+
+//点击Add Task获取input的内容添加新的task
+const bindEventAddTask = function() {
+    const addButton = e('#todo-add')
+    bindEvent(addButton, 'click', function(){
+        //获取input输入框内容
+        const task = e('#todo-input').value
+        log('input content', task)
+        const todo = {
+            'task': task,
+        }
+        //把新内容添加到todoList
+        todoList.push(todo)
+        log(todoList)
+        //保存
+        saveTodos()
+        //插入todo
+        insertTodo(todo)
+    })
+}
+//todo模板
+const todoTemplate = function(todo) {
+    const t = `
+        <div class="todo-item">
+            <i class="fa fa-check-square-o"></i>
+            <p contenteditable="true">${todo.task}</p>
+            <i class="fa fa-trash-o"></i>
+        </div>
+        `
+    return t
+}
+
+const insertTodo = function(todo) {
+    const t = todoTemplate(todo)
+    const todoDiv = e('#todo-content')
+    appendHtml(todoDiv,'beforeend', t)
+}
+
+//保存todoList
+const saveTodos = function() {
+    const s = JSON.stringify(todoList)
+    localStorage.todoList = s
+}
+//读取todoList
+const loadTodos = function() {
+    const s = localStorage.todoList
+    return JSON.parse(s)
+}
+
+//程序加载后把todoList添加到页面中
+const initTodoList = function() {
+    todoList = loadTodos()
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i]
+        insertTodo(todo)
+    }
+}
+//点击垃圾桶图标将task删除
+const bindEventDeleteTask = function() {
+    const todoDiv = e('#todo-content')
+    //1.在父元素上绑定事件
+    bindEvent(todoDiv, 'click', function(event) {
+        const target = event.target
+        const parent = target.parentElement
+        //2.点击时获取到对应的todoList下标
+        const index = indexOfElement(parent, todoDiv.children)
+        log('index', index)
+        //删除todoList对应下标的这一项
+        todoList.splice(index,1)
+        //删掉对应的div的html
+        parent.remove()
+        //3.保存todoList
+        saveTodos()
+    })
+}
+
+
+//添加completed tasks页面内容
+//点击完成图标将task移动到completed tasks
+//点击task内容可以修改并保存
+const __main = function() {
+    showCurrentTime()
+    initTodoList()
+    bindEventAddTask()
+    bindEventDeleteTask()
+}
+
+__main()
